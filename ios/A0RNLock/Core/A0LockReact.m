f06f27809174d7f2ad0e93b50f5ba2274383423f
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-08-20 17:08:30 -0300

    Allow to toggle PKCE

--- ios/A0RNLock/Core/A0LockReact.m
+++ ios/A0RNLock/Core/A0LockReact.m
@@ -76,6 +76,8 @@
         callback(@[@"Please configure Lock before using it", [NSNull null], [NSNull null]]);
         return;
     }
+
+    self.lock.usePKCE = [options[@"pkce"] boolValue];
     NSArray *connections = options[@"connections"];
     NSArray *passwordless = [connections filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(NSString * _Nonnull name, NSDictionary<NSString *,id> * _Nullable bindings) {
         NSString *lowecaseName = name.lowercaseString;

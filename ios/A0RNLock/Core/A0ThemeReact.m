31a5515b9263d94d45ce983af9dd7d16d638e15e
Julien Moutte <julien@ctondemand.com>
2017-01-27 23:19:38 +0100

    Add Image theming.

--- ios/A0RNLock/Core/A0ThemeReact.m
+++ ios/A0RNLock/Core/A0ThemeReact.m
@@ -10,9 +10,14 @@
 
 @implementation A0ThemeReact
 
-- (void)themeLockColorForKey:(NSString *)key style:(NSDictionary *)style {
+- (NSString *)getStyleKeyFromThemeKey:(NSString *)key {
     NSString *tmp = [key substringFromIndex:7];
     NSString *styleKey = [[[tmp substringToIndex:1] lowercaseString] stringByAppendingString:[tmp substringFromIndex:1]];
+    return styleKey;
+}
+
+- (void)themeLockColorForKey:(NSString *)key style:(NSDictionary *)style {
+    NSString *styleKey = [self getStyleKeyFromThemeKey:key];
     NSString *value = style[styleKey];
     if (value) {
         unsigned rgbValue = 0;
@@ -20,13 +25,21 @@
         [scanner setScanLocation:1]; // bypass '#' character
         [scanner scanHexInt:&rgbValue];
         UIColor *color = [UIColor colorWithRed:((rgbValue & 0xFF0000) >> 16)/255.0 green:((rgbValue & 0xFF00) >> 8)/255.0 blue:(rgbValue & 0xFF)/255.0 alpha:1.0];
-        [_theme registerColor:color forKey: key];
+        [_theme registerColor:color forKey:key];
+    }
+}
+
+- (void)themeLockImageForKey:(NSString *)key style:(NSDictionary *)style {
+    NSString *styleKey = [self getStyleKeyFromThemeKey:key];
+    NSString *value = style[styleKey];
+    if (value) {
+        [_theme registerImageWithName:value bundle:NULL forKey:key];
     }
 }
 
 - (void)themeLockWithStyle:(NSDictionary*)style {
     _theme = [[A0Theme alloc] init];
-
+    
     // Primary Button
     [self themeLockColorForKey:@"A0ThemePrimaryButtonNormalColor" style:style];
     [self themeLockColorForKey:@"A0ThemePrimaryButtonHighlightedColor" style:style];
@@ -53,7 +66,18 @@
     [self themeLockColorForKey:@"A0ThemeCredentialBoxBackgroundColor" style:style];
     // Close
     [self themeLockColorForKey:@"A0ThemeCloseButtonTintColor" style:style];
-
+    
+    // Primary Button
+    [self themeLockImageForKey:@"A0ThemePrimaryButtonNormalImageName" style:style];
+    [self themeLockImageForKey:@"A0ThemePrimaryButtonHighlightedImageName" style:style];
+    // Secondary Button
+    [self themeLockImageForKey:@"A0ThemeSecondaryButtonNormalImageName" style:style];
+    [self themeLockImageForKey:@"A0ThemeSecondaryButtonHighlightedImageName" style:style];
+    // Icon
+    [self themeLockImageForKey:@"A0ThemeIconImageName" style:style];
+    // Screen
+    [self themeLockImageForKey:@"A0ThemeScreenBackgroundImageName" style:style];
+    
     [[A0Theme sharedInstance] registerTheme:_theme];
 }
 
